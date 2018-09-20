de760c469e3769f6a1e8eafd66a1d193693490fc
sayaya1090 <sayaya1090@hotmail.com>
2018-09-20 16:19:24 +0900

    ff

---
+++ src/net/sayaya/ngs/toolkit/function/ReadFunction.java
@@ -0,0 +1,9 @@
+package net.sayaya.ngs.toolkit.function;
+
+import java.util.function.Function;
+
+import net.sayaya.ngs.toolkit.data.Read;
+
+public interface ReadFunction<T> extends Function<Read, T> {
+
+}
